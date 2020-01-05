882c63199cde8b78da7ddd92d8ed5b1be66f9d79
Glitch (flat-caraway) <none>
2020-01-05 17:40:27 +0000

    👢👸 Checkpoint ./komutlar/yardım.js:9633130/42

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -10,34 +10,34 @@ exports.run = async(client, message, args) => {
  .setURL("https://discordapp.com/oauth2/authorize?client_id=647386467844227074&scope=bot&permissions=8")
  .setDescription(`
 
-**!yardım**, ile yardım alabilirsiniz.
+**-yardım**, ile yardım alabilirsiniz.
 Örnek komut kullanımı: \`!küfüraç\`
 Botu davet etmek için: \`!davet\`
 `)
- .addField("!komutlar (13)", `
+ .addField("-komutlar (13)", `
 herkesin kullanabileceği standart komutlar;
 \`botbilgi\`,\`davet\`,\`oyver\`,\`gold\`,\`webpanel\`,\`sunucu-pp\`,\`profil\`,\`kredim\`,\`p-market\`,\`p-menü\`,\`puanım\`,\`ayarlar\`
 `)
- .addField("!eğlence (9)", `
+ .addField("-eğlence (9)", `
 herkesin kullanabileceği eğlence komutları;
 \`kralol\`,\`maymunol\`,\`yılanol\`,\`adamol\`,\`kediol\`,\`aşk\`,\`token\`,\`tokat\`
 `)
  
- .addField("!moderasyon (13)", `
+ .addField("-moderasyon (13)", `
 yetkililer için moderasyon komutları;
 \`küfür\`,\`reklam\`,\`link engel\`,\`reklam kick\`,\`reklam isim ban\`,\`oto rol\`,\`sayaç\`,\`ban koruma sistemi\`,\`duyuru\`,\`oto cevap\`,\`mute sistemi\`,\`anti raid bot sistemi\`
 `)
  
- .addField("!moderasyon 2 (12)", `
+ .addField("-moderasyon 2 (12)", `
 yetkililer için moderasyon komutlarının 2. bölümü;
 \`sil\`,\`reklam taraması\`,\`resimli hg bb\`,\`sunucu tanıt\`,\`oto bot silici\`,\`ultra sohbet temizleyici\`,\`yavaş mod\`,\`duyuru\`,\`tag sistemi\`,\`rol sistemi\`,\`geçici oda sistemi\`,\`kayıt sistemi\`
 `)
  
- .addField("!koruma (5)", `
+ .addField("-koruma (5)", `
 Sunucunuz İçin Koruma Sistemleri.
 \`sağ tık ban koruması\`,\`kanal silme koruması\`,\`rol silme koruması\`,\`sunucuya bot sokma koruması\`,\`güvenlik odası\`
 `)
- .setFooter("© Mcadventuretime.com", client.user.avatarURL)
+ .setFooter("SepuLtura", client.user.avatarURL)
  message.channel.send(Embed)
 }
 
