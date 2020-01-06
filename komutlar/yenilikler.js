e7828eecaffad13ce9e77a9b4a076bec6ccda487
Glitch (flat-caraway) <none>
2020-01-06 20:48:24 +0000

    👠🚦 Checkpoint ./komutlar/yenilikler.js:9633130/13

--- komutlar/yenilikler.js
+++ komutlar/yenilikler.js
@@ -6,7 +6,7 @@ exports.run = function(client, message, args) {
     let type = args.slice(0).join(' ');
     if (type.length < 1) return message.channel.send(
 new Discord.RichEmbed()
-.setDescription('  Yenilikler: \n\n **-ping; Komutu Güncelleşti**\n **-duyuru; Komutu Güncelleştirildi** \n **-havadurumu; Komutu Eklendi** \n\n\n'));
+.setDescription('  Yenilikler: \n\n **-ping; Komutu Güncelleştirildi**\n **-duyuru; Komutu Güncelleştirildi** \n **-havadurumu; Komutu Eklendi** \n\n\n'));
 const embed = new Discord.RichEmbed()
 .setColor('RANDOM')
 const embed2 = new Discord.RichEmbed()
