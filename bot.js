beeb5de04b6ccedf06c15e4efad9f96974cd4492
Glitch (flat-caraway) <none>
2020-01-12 10:25:55 +0000

    🌂🌿 Checkpoint ./bot.js:9633130/277 ./otocevap.json:9633130/486

--- bot.js
+++ bot.js
@@ -472,6 +472,23 @@ client.channels.get(log).send(`Sunucundan bir yetkili ban limitine ulaştı ve s
 })
 
 
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+		if (!msg.guild.member(msg.author).hasPermission("BAN_MEMBERS")) {
+			msg.author.sendMessage('Aleyküm selam,  hoş geldin ^^');
+		} else {
+		msg.reply('Aleyküm selam, hoş geldin ^^');
+		}
+	}
+});
+
+
+
+
+
+
+
 //Kanal Limit
 client.on("channelDelete", async(channel) => {
   const guild = channel.guild;
